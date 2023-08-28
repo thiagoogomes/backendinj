73e3d2371036d193f575d839dadedea393863ddf
thiagoogomes <teagomes87@gmail.com>
2023-08-28 17:58:57 -0300

    teste

--- index.js
+++ index.js
@@ -1,10 +1,5 @@
 import express from 'express';
 import cors from 'cors'
-import rotaIntegrante from "./Routes/rotaIntegrante.js";
-import rotaEvento from "./Routes/rotaEvento.js";
-import rotaMusica from './Routes/rotaMusica.js';
-import rotaMaterial from './Routes/rotaMaterial.js';
-import rotaFuncao from './Routes/rotaFuncao.js';
 
 const app = express();
 app.use(cors({origin:"*"}));
